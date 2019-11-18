8b74ba531487229da12587d542b8d7321875608b
danielbengtsen <cph-db145@cphbusiness.dk>
2019-11-18 10:08:59 +0100

    done

--- src/sprogskift/SprogSkift.java
+++ src/sprogskift/SprogSkift.java
@@ -2,7 +2,7 @@
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
-*/
+ */
 package sprogskift;
 
 import java.util.Scanner;
@@ -12,38 +12,44 @@ import java.util.Scanner;
  * @author thor
  */
 public class SprogSkift {
-	
-	/**
-	 * @param args the command line arguments
-	 */
-	static Dansk sprog;
-	static Scanner sc;
-	public static void main(String[] args) {
-		int choice = 0;
-		sc = new Scanner(System.in);
-		sprog = new Dansk();
-		sprog.printMenu();
-		while (choice != 9) {
-			choice = sc.nextInt();
-			switch(choice) {
-				case 4:
-					changeLanguage();
-					break;
-				default:
-					choice = 9;
-					break;
-			}
-		}
-		// print en menu
-	}
-	public static void changeLanguage() {
-		sprog.printLang(); 
-		int langchoice = sc.nextInt();
-		switch(langchoice) {
-			case 1: sprog = new Dansk();break;
-			case 2: sprog = new Engelsk();break;
-		}
-		
-	}
-	
+
+    public static SprogI sprog = new Dansk();
+    static Scanner sc;
+
+    public static void main(String[] args) {
+        int choice = 0;
+        sc = new Scanner(System.in);
+        sprog = new Dansk();
+        while (choice != 9) {
+            sprog.printMenu();
+            choice = sc.nextInt();
+            switch (choice) {
+                case 4:
+                    changeLanguage();
+                    break;
+                default:
+                    choice = 9;
+                    break;
+            }
+        }
+        // print en menu
+    }
+
+    public static void changeLanguage() {
+        sprog.printLang();
+        int langchoice = sc.nextInt();
+        switch (langchoice) {
+            case 1:
+                sprog = new Dansk();
+                break;
+            case 2:
+                sprog = new Engelsk();
+                break;
+            case 3:
+                sprog = new Volapyk();
+                break;
+        }
+
+    }
+
 }
---
+++ src/sprogskift/Volapyk.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprogskift;
+
+public class Volapyk implements SprogI {
+    
+    public void printMenu() {
+		System.out.println("Hardnatachi");
+		System.out.println("1) hui harnividi");
+		System.out.println("2) kria harnividi");
+		System.out.println("3) shi harnividi mu gigi");
+		System.out.println("4) chikr nte du");
+		System.out.println("9) howoui");
+		System.out.println("hablably: ");
+	}
+	public void printLang() {
+		System.out.println("hablublybi: ");
+		System.out.println("1) Duwé");
+		System.out.println("2) Chikr");
+                System.out.println("3) Volpyki");
+	}
+    
+}
